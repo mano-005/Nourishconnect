fec7a4cfde26c6ef9d6f69e64382ef63f9ad0fb8
mano <manoraj070@gmail.com>
2026-09-08 20:31:51 +0530

    prepare for deployment

--- frontend/reactproject/vite.config.js
+++ frontend/reactproject/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true
+    host: true,
+    base:'/Nourishconnect/'
   },
   build: {
     rollupOptions: {
